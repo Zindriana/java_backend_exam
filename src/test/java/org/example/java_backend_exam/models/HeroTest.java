04a07c6607d6a1cbf102ec304695750c7f2eec01
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-06 20:21:15 +0100

    updated the test to check if the name had spaces before and after the name

--- src/test/java/org/example/java_backend_exam/models/HeroTest.java
+++ src/test/java/org/example/java_backend_exam/models/HeroTest.java
@@ -10,11 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class HeroTest {
 
     @Test
-    void createHeroTest() {
+    void createHeroBasicTest() {
         List<String> languages = new ArrayList<>();
         languages.add("human");
         languages.add("dwarven");
-        Hero hero = new Hero("Hero name", languages);
+        Hero hero = new Hero(" Hero name ", languages);
         assertNotNull(hero);
         assertEquals(hero.getName(), "Hero name");
         assertEquals(hero.getLanguages().size(), 2);
