413720e5ee528b652ae089653607e65687ffa34a
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-06 16:39:58 +0100

    added a test for creating a hero

---
+++ src/test/java/org/example/java_backend_exam/models/HeroTest.java
@@ -0,0 +1,24 @@
+package org.example.java_backend_exam.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @Test
+    void createHeroTest() {
+        List<String> languages = new ArrayList<>();
+        languages.add("human");
+        languages.add("dwarven");
+        Hero hero = new Hero("Hero name", languages);
+        assertNotNull(hero);
+        assertEquals(hero.getName(), "Hero name");
+        assertEquals(hero.getLanguages().size(), 2);
+        assertTrue(hero.getLanguages().contains("dwarven"));
+    }
+
+}
