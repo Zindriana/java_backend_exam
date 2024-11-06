7ee3f7b3e6e026be2582f75de69a77528c07249f
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-06 21:26:45 +0100

    updated the constructor (deleted the autowired).
    added return heroList in /getheroes
    
    added heroList.add(hero) in /createhero

--- src/main/java/org/example/java_backend_exam/Controller.java
+++ src/main/java/org/example/java_backend_exam/Controller.java
@@ -1,30 +1,25 @@
 package org.example.java_backend_exam;
 
-import org.example.java_backend_exam.lists.HeroList;
-import org.example.java_backend_exam.lists.ScrollList;
 import org.example.java_backend_exam.models.Hero;
 import org.example.java_backend_exam.models.ReadAndWrite;
 import org.example.java_backend_exam.models.Scroll;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Controller {
-    private final HeroList heroList;
-    private final ScrollList scrollList;
+    private ArrayList<Hero> heroList = new ArrayList<>();
+    private ArrayList<Scroll> scrollList = new ArrayList<>();
     private final ReadAndWrite language = new ReadAndWrite();
 
-    @Autowired
-    public Controller(HeroList heroList, ScrollList scrollList) {
-        this.heroList = heroList;
-        this.scrollList = scrollList;
-    }
+    public Controller() {}
 
     @CrossOrigin(origins = "https://zindriana.github.io/")
     @PostMapping("/createhero")
     public void CreateHero(@RequestBody Hero hero){
+        heroList.add(hero);
     }
 
     @PostMapping("/createscroll")
@@ -34,7 +29,7 @@ public class Controller {
 
     @GetMapping("/getheroes")
     public List<Hero> getHeroes(){
-        return null;
+        return heroList;
     }
 
     @GetMapping("/getscrolls")
