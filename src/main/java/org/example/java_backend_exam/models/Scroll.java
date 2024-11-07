fe3d47e5cf23cc7e4be6cd1144115719683843d1
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-07 12:54:30 +0100

    added fields, constructor and getters

--- src/main/java/org/example/java_backend_exam/models/Scroll.java
+++ src/main/java/org/example/java_backend_exam/models/Scroll.java
@@ -1,4 +1,27 @@
 package org.example.java_backend_exam.models;
 
 public class Scroll {
+
+    private String name;
+    private String language;
+    private String content;
+
+
+    public Scroll(String name, String language, String content) {
+        this.name = name.trim();
+        this.language = language.trim().toLowerCase();
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
