8b1719bcbd504b8b6af0f706b2aeec824c2eb655
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-07 21:22:53 +0100

    Added decryption

--- src/main/java/org/example/java_backend_exam/models/ReadAndWrite.java
+++ src/main/java/org/example/java_backend_exam/models/ReadAndWrite.java
@@ -26,6 +26,28 @@ public class ReadAndWrite {
         return scroll;
     }
 
+    public Scroll decryptScroll(Scroll scroll) {
+        String language = scroll.getLanguage();
+        Scroll decryptedScroll = scroll;
+        switch (language) {
+            case "dwarven":
+                decryptedScroll.setContent(decryptDwarven(scroll.getContent()));
+                return decryptedScroll;
+            case "elven":
+                decryptedScroll.setContent(decryptElven(scroll.getContent()));
+                return decryptedScroll;
+            case "human":
+                decryptedScroll.setContent(decryptHuman(scroll.getContent()));
+                return decryptedScroll;
+            case "orc":
+                decryptedScroll.setContent(decryptOrc(scroll.getContent()));
+                return decryptedScroll;
+            default:
+                break;
+        }
+        return scroll;
+    }
+
     private String encryptDwarven(String text) {
         StringBuilder encryptString = new StringBuilder();
         String trimmedText = text.trim();
@@ -54,9 +76,8 @@ public class ReadAndWrite {
     private String encryptHuman(String text) {
         StringBuilder stringBuilder = new StringBuilder(text);
         String reversedString = stringBuilder.reverse().toString();
-        String lowCaseString = reversedString.toLowerCase();
         StringBuilder encryptedString = new StringBuilder();
-        for (char c : lowCaseString.toCharArray()) {
+        for (char c : reversedString.toCharArray()) {
             if (c == ' ') {
                 encryptedString.append(rand.nextInt(10));
             } else {
@@ -73,6 +94,62 @@ public class ReadAndWrite {
                 char shifted = (char) (c + 1);
                 if ((Character.isLowerCase(c) && shifted > 'z') || (Character.isUpperCase(c) && shifted > 'Z')) {
                     shifted -= 26;
+                }
+                encryptString.append(shifted);
+            } else {
+                encryptString.append(c);
+            }
+        }
+        return encryptString.toString().trim();
+    }
+
+    private String decryptDwarven(String encryptedText) {
+        StringBuilder decryptedString = new StringBuilder();
+        String[] asciiValues = encryptedText.split(" ");
+
+        for (String asciiValue : asciiValues) {
+            int charCode = Integer.parseInt(asciiValue);
+            decryptedString.append((char) charCode);
+        }
+
+        return decryptedString.toString();
+    }
+
+    private String decryptElven(String text) {
+        StringBuilder encryptString = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char shifted = (char) (c + 1);
+                if ((Character.isLowerCase(c) && shifted > 'z') || (Character.isUpperCase(c) && shifted > 'Z')) {
+                    shifted -= 26;
+                }
+                encryptString.append(shifted);
+            } else {
+                encryptString.append(c);
+            }
+        }
+        return encryptString.toString();
+    }
+
+    private String decryptHuman(String text) {
+        StringBuilder decryptedString = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isDigit(c)) {
+                decryptedString.append(' ');
+            } else {
+                decryptedString.append(c);
+            }
+        }
+        return decryptedString.reverse().toString();
+    }
+
+    private String decryptOrc(String text) {
+        StringBuilder encryptString = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char shifted = (char) (c - 1);
+                if ((Character.isLowerCase(c) && shifted < 'a') || (Character.isUpperCase(c) && shifted < 'A')) {
+                    shifted += 26;
                 }
                 encryptString.append(shifted);
             } else {
